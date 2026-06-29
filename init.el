8833302502cf6f9701c11f3dcf35f81c026b91df
hby <hbystuff@gmail.com>
2026-06-29 13:25:55 -0700

    File goto etc

--- init.el
+++ init.el
@@ -2,18 +2,19 @@
 ;; Basic stuff. These should not depend on any thing; even if
 ;; packages fail to load, they shouldn't fail.
 
-;; Increase garbage collection threshold during startup; reset after
+;; Perf stuff
 (setq gc-cons-threshold 100000000)
 (add-hook 'emacs-startup-hook
           (lambda () (setq gc-cons-threshold 800000)))
-
 (setq inhibit-startup-screen t)
 (menu-bar-mode -1)
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
+;; Looks and feels
 (load-theme 'modus-vivendi t)
 (set-frame-font "Consolas 12" nil t)
+
 ;; Disable backup and auto-save files completely
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -28,6 +29,10 @@
 (setq custom-file (concat user-emacs-directory "custom-vars.el"))
 (load custom-file 'noerror)
 
+;; Bind it to a Vim-like combo (Space-f) in Normal Mode
+(with-eval-after-load 'evil
+  (define-key evil-normal-state-map (kbd "SPC f") 'find-file))
+
 ;;===========================================
 ;; Package manager
 (require 'package)
@@ -36,9 +41,10 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Install mode
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+;; Install packages
+(unless (package-installed-p 'evil) (package-install 'evil))
+(unless (package-installed-p 'vertico) (package-install 'vertico))
+(unless (package-installed-p 'orderless) (package-install 'orderless))
 
 ;;===========================================
 ;; Evil mode setup
@@ -49,6 +55,17 @@
 (setq evil-split-window-below t)
 (setq evil-want-C-u-scroll t)
 (setq evil-undo-system 'undo-redo)
+
+;;===========================================
+;; File goto
+(require 'vertico)
+(vertico-mode 1)
+(require 'orderless)
+(setq completion-styles '(orderless basic)
+      completion-category-defaults nil
+      completion-category-overrides '((file (styles partial-completion))))
+(with-eval-after-load 'evil
+  (define-key evil-normal-state-map (kbd "C-p") 'find-file))
 
 (require 'evil)
 (evil-mode 1)
